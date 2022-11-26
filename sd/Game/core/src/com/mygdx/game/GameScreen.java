c63a99f672641f8e264ad6f5af5f36fd04ab8f11
JamesBT <c14210026@john.petra.ac.id>
2022-11-26 17:24:52 +0700

    gamescreen (1)

---
+++ sd/Game/core/src/com/mygdx/game/GameScreen.java
@@ -0,0 +1,13 @@
+package com.mygdx.game;
+
+public class GameScreen extends BaseScreen{
+
+    public GameScreen(BaseGame g) {
+
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
+}
