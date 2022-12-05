68d5488bc6349f8023c07ddb869c5834c60720f0
JamesBT <c14210026@john.petra.ac.id>
2022-12-05 18:41:20 +0700

    graph, bfs

--- sd/Game/core/src/com/mygdx/game/Graph.java
+++ sd/Game/core/src/com/mygdx/game/Graph.java
@@ -1,10 +1,6 @@
 package com.mygdx.game;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Arrays.*;
+import java.util.*;
 
 public class Graph<T>{
     private int adjMatrix[][];
@@ -22,22 +18,65 @@ public class Graph<T>{
         adjMatrix[j][i] = 1;
     }
 
-    // Print the matrix
-//    public String toString() {
-//        StringBuilder s = new StringBuilder();
-//        for (int i = 0; i < this.size; i++) {
-//            s.append(i + ": ");
-//            for (int j : adjMatrix[i]) {
-//                s.append((j ? 1 : 0) + " ");
-//            }
-//            s.append("\n");
-//        }
-//        return s.toString();
-//    }
+    public void printgraph(){
+        for(int[] i : adjMatrix){
+            for(int j : i){
+                System.out.print(j+" ");
+            }
+            System.out.println("");
+        }
+    }
 
-    //bfs
-//    public int[] bfs(int awal,int tujuan){
-//        ArrayList<Integer> queue = new ArrayList<Integer>();
-//
-//    }
+    public void shortestpath(int awal, int dest){
+        int pred[] = new int[this.size];
+        if(BFS(awal,dest,pred) == false){
+            System.out.println("error");
+            return;
+        }
+
+        ArrayList<Integer> path = new ArrayList<Integer>();
+        int crawl=dest;
+        path.add(crawl);
+        while(pred[crawl] != -1){
+            path.add(pred[crawl]);
+            crawl=pred[crawl];
+        }
+
+        System.out.print("Path: ");
+        for(int i=path.size() - 1 ;i >= 0;i--){
+            System.out.print(path.get(i)+" ");
+        }
+    }
+
+    private boolean BFS(int awal, int dest, int pred[]){
+        ArrayList<Integer> queue = new ArrayList<Integer>();
+        boolean visited[] = new boolean[this.size];
+
+        for(int i=0;i<this.size;i++){
+            visited[i] = false;
+            pred[i] = -1;
+        }
+
+        visited[awal] = true;
+        queue.add(awal);
+
+        while(!queue.isEmpty()){
+            int u = queue.get(0);
+            queue.remove(0);
+            for(int i=0;i<adjMatrix[u].length;i++){
+                if(adjMatrix[u][i] == 1 && visited[i] == false){
+                    visited[i] = true;
+                    pred[i] = u;
+                    queue.add(i);
+
+                    if(adjMatrix[u][i] == 1 && i==dest){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
 }
